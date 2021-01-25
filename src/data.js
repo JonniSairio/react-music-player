be83fda0a8fb75cd83796cee4a3cfa10953cb5ab
jonnisairio <jonni.sairio@netti.fi>
2021-01-25 21:55:13 +0200

    Project Added

---
+++ src/data.js
@@ -0,0 +1,105 @@
+import { v4 as uuidv4 } from "uuid";
+
+
+function chillHop() {
+  return [
+    {
+      name: "Entropy",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/a8e5b36f-e65f-4b45-96ba-63f35a286bef/artwork-440x440.jpg",
+      artist: "Distrion, Alex Skrindo",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/162/entropy-1586947085-nMePSh9jHT.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Hollywood Perfect",
+      cover: "https://linkstorage.linkfire.com/medialinks/images/612c5806-8a6b-4ddd-bd1b-4928da9ca12d/artwork-440x440.jpg",
+      artist: "Unknown Brain, NotEvenTanner",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/855/hollywood-perfect-1606402830-Y7OnatOVsT.mp3",
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Sky High pt. II",
+      cover: "https://linkstorage.linkfire.com/medialinks/images/5292ff87-2250-48f9-977e-e8ed2a8da072/artwork-440x440.jpg",
+      artist: "Elektronomia",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/887/sky-high-pt-ii-1609495231-50f9lRx1Nv.mp3",
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Sun Goes Down",
+      cover: "https://linkstorage.linkfire.com/medialinks/images/28b808fe-951c-427c-bef5-2d0b609bb491/artwork-440x440.jpg",
+      artist: "ROY KNOX, Jim Yosef",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/754/sun-goes-down-1597319232-W7UYbdhijy.mp3",
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Más Y Máss",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/0ec84251-f2bc-4908-997f-cc94d27e3622/artwork-440x440.jpg",
+      artist: "Cartoon",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/707/mas-y-mas-1586960824-GsD6dk7dUy.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ignite",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/b275709b-bf6f-4f66-82c5-0ac8020b1dfb/artwork-440x440.jpg",
+      artist: "Tom Wilson, Heuse",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/826/ignite-1600419629-48VJLcO6hO.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "I Just Wanna",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/d8fb82bf-96b6-4cfd-8599-69a351999773/artwork-440x440.jpg",
+      artist: "NIVIRO",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/522/i-just-wanna-1586955474-GhanmVetWX.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Link",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/85803633-60c7-45b2-9819-239964911c37/artwork-440x440.jpg",
+      artist: "Jim Yosef",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/356/link-1586950573-1rqR7whieT.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Passion",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/0ed58974-c096-4b20-bc2f-cd00d29c8d31/artwork-440x440.jpg",
+      artist: "Jim Yosef, Alex Skrindo",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/145/passion-1586946956-Dk0A4ui89X.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Bubble",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/9583e63f-21c3-4499-8e79-c1f4ad686310/artwork-440x440.jpg",
+      artist: "Raven & Kreyn",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/472/bubble-1586954276-Q1Wzsa1ivi.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Joystick",
+      cover:
+        "https://linkstorage.linkfire.com/medialinks/images/66a25a6c-09ca-40bb-94ba-7f2d216f3751/artwork-440x440.jpg",
+      artist: "Jensation",
+      audio: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/300/joystick-1586949005-DulqjtNoUl.mp3",
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
